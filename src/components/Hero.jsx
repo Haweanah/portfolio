4604619480debafde9c343d457015a789c41821a
Haweanah <ahauwa48@yahoo.com>
2026-06-08 17:06:00 +0100

    Add data to hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,28 @@
 export default function Hero () {
     return (
-        <>
-        </>
+        <div className="hero">
+            <div className="hero-left">
+                <h1>
+                    Hauwa Abdulkadir
+                </h1>
+                <p className="para-top">
+                    Your go-to engineer for Web Development projects
+                    </p>
+                    <p className="pata-bottom">
+                       I'm a Microbiologist turned Software Engineer.
+                        Building technology solutions for healthcare,
+                        food safety, and water quality.
+                        Bringing your ideas to life with clean, efficient and scalable code.
+                    </p>
+                    <div className="hero-links">
+                        <button>
+                            <a href="">Contact me</a>
+                            </button><button>View projects</button>
+                    </div>
+            </div>
+            <div className="hero-right">
+                <img src="./images/frontview.png" alt="profile photo" />
+            </div>
+        </div>
     )
 }
