f8e40463f9fae8ebb7e02228800f1619ec220697
Haweanah <ahauwa48@yahoo.com>
2026-06-15 12:56:14 +0100

    Update projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "Responsive weather application built with React.",
     technologies: ["React", "CSS", "Weather API"],
-    image: "/images/weatherpage.png",
+    image: "/images/weatherpagemobile.png",
     imagedesktop: "/images/weatherpage.png",
     github:
       "https://github.com/Haweanah/weather-app",
@@ -19,11 +19,26 @@ export const projects = [
     description:
       "Note-taking application built with Flask.",
     technologies: ["Python", "Flask", "SQLite"],
-    image: "/images/notenique.png",
+    image: "/images/noteniquemobile.png",
     imagedesktop: "/images/notenique.png",
     github:
       "https://github.com/Haweanah/notenique",
     live:
       "https://notenique.vercel.app/",
+  },
+
+
+  {
+    id: 2,
+    title: "Product-list-with-cart",
+    description:
+      "A responsive product list and shopping cart app built with React.",
+    technologies: ["CSS", "React",],
+    image: "/images/productlistmobile.png",
+    imagedesktop: "/images/productlistdesktop.png",
+    github:
+      "https://github.com/Haweanah/product-list-with-cart/",
+    live:
+      "https://product-list-with-cart-15jy.onrender.com/",
   },
 ];
