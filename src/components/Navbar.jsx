27ed0f85f745130b0a7c1e1833e03e4c285f4bf2
Haweanah <ahauwa48@yahoo.com>
2026-06-16 11:47:56 +0100

    Make navbar sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,15 +9,15 @@ export default function Navbar () {
                 <div className="navbar-right">
                     <li><a href="#navbar">Home</a></li>
                     <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Projects</a></li>
                     <li><a href="#skills">Skills</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                     <li><a href="#experience">Experience</a></li>
                     <li><a href="#contact">Contact</a></li>
                     <li>
                         <a
                             href="/resume.pdf"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="resume-nav"
                             >
                             Resume
                             </a>
