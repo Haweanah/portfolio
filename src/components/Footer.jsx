b1a71dc6b8192a993baca8e35022d1e113e77653
Haweanah <ahauwa48@yahoo.com>
2026-06-16 11:28:47 +0100

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
-export default function Footer () {
-    return (
-        <></>
-    )
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Designed & Developed by Hauwa Abdulkadir
+      </p>
+
+      <p className="copyright">
+        © 2026 All Rights Reserved
+      </p>
+    </footer>
+  );
 }
